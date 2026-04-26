0d0ca1ac99afff9a70165ae197421c930af1624e
nadir <layoune95@gmail.com>
2026-04-26 15:02:49 +0100

    Fix build: Downgrade reanimated to stable v3 and clean worklets

--- babel.config.js
+++ babel.config.js
@@ -3,8 +3,6 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      // react-native-reanimated plugin (miroir de la config standard)
-      // En v4, ce plugin requiert 'react-native-worklets/plugin'
       'react-native-reanimated/plugin',
     ],
   };
